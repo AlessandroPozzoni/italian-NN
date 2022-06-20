b5c9d6aa70e8682b9bb6d1f1feb3ffe5a04fbbdd
AlessandroPozzoni <alessandro1.pozzoni@mail.polimi.it>
2022-06-21 01:16:12 +0200

    Added a delayed selfie function

--- italian-NN/predictor.py
+++ italian-NN/predictor.py
@@ -5,8 +5,18 @@ import time
 import os
 import numpy as np
 from PIL import Image
+import glob
 
 FRAME_SIZE = (256, 256)
+
+list_of_files = glob.glob(os.path.join(os.path.dirname(os.path.abspath(__file__)), "selfportrait\\" + "*.jpg"))
+if list_of_files:
+    latest_file = max(list_of_files, key=os.path.getctime)
+    last = int(latest_file.split("\\selfportrait\\")[1].split('.')[0])
+else:
+    last = -1
+
+last += 1
 
 classes = ["me", "nothing", "victory", "yes"]
 
@@ -19,15 +29,15 @@ model = keras.models.load_model(os.path.join(os.path.dirname(os.path.abspath(__f
 
 v = 0
 
-while v < 5:
+while 1:
         ret, frame = cap.read()
-        frame = cv2.resize(frame, FRAME_SIZE)
-        cv2.imshow("Acquisition", frame)
+        img = cv2.resize(frame, FRAME_SIZE)
+        cv2.imshow("Acquisition", img)
         cv2.waitKey(1)
-        frame = Image.fromarray(frame)
-        frame = np.asarray(frame)
-        frame = tf.expand_dims(frame, 0)
-        prediction = model.predict(frame)
+        img = Image.fromarray(img)
+        img = np.asarray(img)
+        img = tf.expand_dims(img, 0)
+        prediction = model.predict(img)
         pred_class = classes[np.argmax(prediction[0])]
         pred_prec = 100 * np.max(prediction[0])
         if pred_class == "victory":
@@ -37,4 +47,10 @@ while v < 5:
 
         print(pred_class, pred_prec, v)
 
-os.startfile("C:/Users/aless/AppData/Local/Discord/app-1.0.9005/Discord.exe")
+        if v >= 5:
+            time.sleep(3)
+            path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "selfportrait\\" + str(last) + ".jpg")
+            cv2.imwrite(filename=path, img=frame)
+            last += 1
+
+
